9b8b054e31cd9d2a4ab718a256046346bf244729
priya282000 <86100193+priya282000@users.noreply.github.com>
2021-06-21 20:13:24 +0530

    Update p7.py

--- Exercise/Program 7/p7.py
+++ Exercise/Program 7/p7.py
@@ -1,10 +1,42 @@
 try:
-    name=input("Name : ")
-    age=int(input("Age : "))
-    gender=input("Gender : ")
-    salary=float(input("Salary : "))
-    state=input("State : ")
-    city=input("City : ")
+    try:
+        name=input("Name : ")
+        if not isinstance(name,str):
+            raise TypeError("Name should contain only alphabets")
+    except TypeError as e:
+        print(e)
+    try:
+        age=int(input("Age : "))
+        #check=isinstance(age,int)
+        if not isinstance(age,int):
+            raise TypeError("Age should contain only numbers")
+    except TypeError as e:
+        print(e)
+    try:
+        gender=input("Gender : ")
+        if not isinstance(gender,str):
+            raise TypeError("Gender should contain only alphabets")
+    except TypeError as e:
+        print(e)
+    try:
+        salary=float(input("Salary : "))
+        if not isinstance(salary,float):
+            raise TypeError("Salary should contain only floating point numbers")
+    except TypeError as e:
+        print(e)
+    try:
+        state=input("State : ")
+        if not isinstance(state,str):
+            raise TypeError("State should contain only alphabets")
+    except TypeError as e:
+        print(e)
+    try:
+        city=input("City : ")
+        if not isinstance(city,str):
+            raise TypeError("City should contain only alphabets")
+    except TypeError as e:
+        print(e)
+
     print("Name :",name,"\nAge :",age,"\nGender :",gender,"\nSalary :",salary,"\nState :",state,"\nCity :",city)
 except Exception as e:
     print(e)
